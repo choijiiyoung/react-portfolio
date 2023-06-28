e6490119f6f1f98da0a3b965b094885faa32de9c
choijiiyoung <blueberryxchoco@gmail.com>
2023-06-28 18:40:24 +0900

    헤더 링크 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -33,8 +33,8 @@ function Header() {
 							</NavLink>
 						</li>
 						<li>
-							<NavLink to='/members' activeClassName={active}>
-								Members
+							<NavLink to='/member' activeClassName={active}>
+								Member
 							</NavLink>
 						</li>
 						<li>
