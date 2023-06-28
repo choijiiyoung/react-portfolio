583d196b4572db3fc9b09ee1ee33aae78ba0c043
choijiiyoung <blueberryxchoco@gmail.com>
2023-06-28 23:40:49 +0900

    index.js 수정

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-	<HashRouter>
-		<App />
-	</HashRouter>,
+	<React.StrictMode>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</React.StrictMode>,
 	document.getElementById('root')
 );
