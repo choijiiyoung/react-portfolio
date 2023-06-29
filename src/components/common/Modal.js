84860e85b4e9bc81c1a20665b2aaf9ed83933575
choijiiyoung <blueberryxchoco@gmail.com>
2023-06-30 00:21:12 +0900

    유튜브 iframe

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -2,6 +2,7 @@ import { forwardRef, useImperativeHandle, useState } from 'react';
 
 const Modal = forwardRef((props, ref) => {
 	const [Open, setOpen] = useState(false);
+	console.log(props);
 
 	useImperativeHandle(ref, () => {
 		return { open: () => setOpen(true) };
