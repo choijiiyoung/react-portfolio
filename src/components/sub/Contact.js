7e13a3522c9b0ff3e5043a76011d506a5964a11c
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-01 23:48:01 +0900

    카카오맵 마커

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -4,14 +4,18 @@ import Layout from '../common/Layout';
 function Contact() {
 	const container = useRef(null);
 	const { kakao } = window;
-
 	const option = {
 		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
 		level: 3, // 지도의 확대 레벨
 	};
 
 	useEffect(() => {
-		new kakao.maps.Map(container.current, option);
+		const mapInstance = new kakao.maps.Map(container.current, option);
+		const marker = new kakao.maps.Marker({
+			position: option.center,
+		});
+
+		marker.setMap(mapInstance);
 	}, []);
 
 	return (
