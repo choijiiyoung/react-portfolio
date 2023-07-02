7c7d52ad04155c966f0cf8f45d7d0312667cdd22
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-03 00:01:33 +0900

    메인 상단 비주얼 추가

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,5 +1,48 @@
+import { Link } from 'react-router-dom';
+
 function Visual() {
-	return <div>Visual</div>;
+	return (
+		<>
+			<figure id='visual' className='my_scroll on'>
+				<div className='inner'>
+					<div className='title_wrap'>
+						<h1>
+							<span className='title'>Hear.</span>
+							<span className='title'>See.</span>
+							<span className='title small'>
+								<span>Like nothing</span>
+								<span>you've heard before</span>
+								<span className='line'></span>
+							</span>
+							<span className='title last'>Feel.</span>
+						</h1>
+					</div>
+
+					{/* <article className='pic'>
+						<img src={process.env.PUBLIC_URL + 'img/main/visual.jpg'} alt='visual' />
+					</article>
+
+					<div className='txt_wrap'>
+						<div className='txt'>
+							<div className='desc'>
+								<p>
+									Lorem, ipsum dolor sit amet consectetur <span>adipisicing elit.</span>
+								</p>
+								<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+							</div>
+							<div className='cross'>
+								<span className='bar row'></span>
+								<span className='bar col'></span>
+							</div>
+						</div>
+						<Link to='/' className='btn'>
+							DISCOVER
+						</Link>
+					</div> */}
+				</div>
+			</figure>
+		</>
+	);
 }
 
 export default Visual;
