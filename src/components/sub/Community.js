051f8af94952f09e077560b26248e42397fad2d4
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-04 21:00:56 +0900

    create 기능 작업 중

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,37 @@
 import Layout from '../common/Layout';
+import { useRef, useState, useEffect } from 'react';
 
 function Community() {
-	return <Layout name={'Community'}>Community</Layout>;
+	const input = useRef(null);
+	const textarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	const createPost = () => {
+		setPosts([...Posts, { title: input.current.value, content: textarea.current.value }]);
+	};
+
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
+
+	return (
+		<Layout name={'Community'}>
+			<section>
+				<div className='inner'>
+					<div className='input_box'>
+						<input type='text' placeholder='제목을 입력하세요.' ref={input} />
+						<br />
+						<textarea cols='30' rows='3' placeholder='본문을 입력하세요.' ref={textarea}></textarea>
+
+						<div className='btn_wrap'>
+							<button>cancle</button>
+							<button onClick={createPost}>wirte</button>
+						</div>
+					</div>
+				</div>
+			</section>
+		</Layout>
+	);
 }
 
 export default Community;
