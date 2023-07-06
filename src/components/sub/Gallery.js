97cb6e8728a64977e781908fd7b56fd436c97501
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-06 18:10:44 +0900

    갤러리 로딩추가

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,38 +1,50 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
 import Masonry from 'react-masonry-component';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
+	const frame = useRef(null);
 	const [Items, setItems] = useState([]);
+	const [Loader, setLoader] = useState(true);
 
 	const getFlickr = async (opt) => {
 		const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1`;
 		const key = 'db5673d91b2fb6704d13f6b0181efd99';
 		const method_interest = 'flickr.interestingness.getList';
-		const method_search = 'flickr.photos.search';
 		const method_user = 'flickr.people.getPhotos';
-		// const myId = '198483448@N02';
 		const num = 20;
 		let url = '';
+		let counter = 0;
 
 		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
-		if (opt.type === 'search')
-			url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
 		if (opt.type === 'user')
 			url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
 		const result = await axios.get(url);
 		setItems(result.data.photos.photo);
+
+		const imgs = frame.current.querySelectorAll('img');
+		imgs.forEach((img, idx) => {
+			img.onload = () => {
+				++counter;
+
+				if (counter === imgs.length) {
+					setLoader(false);
+					frame.current.classList.add('on');
+				}
+			};
+		});
 	};
 
+	// useEffect(() => getFlickr({ type: 'user', user: '198483448@N02' }), []);
 	useEffect(() => getFlickr({ type: 'interest' }), []);
 
 	return (
 		<Layout name={'Gallery'}>
 			<section>
 				<div className='inner'>
-					<div className='frame'>
+					<div className='frame' ref={frame}>
 						<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
 							{Items.map((item, idx) => {
 								return (
@@ -60,6 +72,7 @@ function Gallery() {
 							})}
 						</Masonry>
 					</div>
+					{Loader && <img className='loader' src={`${process.env.PUBLIC_URL}/img/gallery/loading.gif`} alt='loader' />}
 				</div>
 			</section>
 		</Layout>
