f70e072be2f7ff233a96af5d038094ff26caf3f5
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-09 21:30:54 +0900

    모바일 메뉴 컴포넌트 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,76 +3,82 @@ import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
 import { faBagShopping, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, NavLink } from 'react-router-dom';
+import Menu from './Menu';
+import { useRef } from 'react';
 
 function Header() {
+	const toggleMenu = useRef(null);
 	const active = 'on';
 	return (
-		<header>
-			<div className='header_inner'>
-				<h1 className='logo'>
-					<Link to='/'>
-						<FontAwesomeIcon icon={faEnvira} />
-					</Link>
-				</h1>
+		<>
+			<header>
+				<div className='header_inner'>
+					<h1 className='logo'>
+						<Link to='/'>
+							<FontAwesomeIcon icon={faEnvira} />
+						</Link>
+					</h1>
 
-				<nav>
-					<ul id='gnb'>
+					<nav>
+						<ul id='gnb'>
+							<li>
+								<NavLink to='/department' activeClassName={active}>
+									Department
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/youtube' activeClassName={active}>
+									Youtube
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/gallery' activeClassName={active}>
+									Gallery
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/member' activeClassName={active}>
+									Member
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/contact' activeClassName={active}>
+									Contact
+								</NavLink>
+							</li>
+							<li>
+								<NavLink to='/community' activeClassName={active}>
+									Community
+								</NavLink>
+							</li>
+						</ul>
+					</nav>
+
+					<ul className='util'>
 						<li>
-							<NavLink to='/department' activeClassName={active}>
-								Department
-							</NavLink>
+							<Link to='/etc'>
+								<FontAwesomeIcon icon={faHeart} />
+							</Link>
 						</li>
 						<li>
-							<NavLink to='/youtube' activeClassName={active}>
-								Youtube
-							</NavLink>
+							<Link to='/etc2'>
+								<FontAwesomeIcon icon={faUser} />
+							</Link>
 						</li>
 						<li>
-							<NavLink to='/gallery' activeClassName={active}>
-								Gallery
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/member' activeClassName={active}>
-								Member
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/contact' activeClassName={active}>
-								Contact
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to='/community' activeClassName={active}>
-								Community
-							</NavLink>
+							<Link to='/etc3'>
+								<FontAwesomeIcon icon={faBagShopping} />
+							</Link>
 						</li>
 					</ul>
-				</nav>
 
-				<ul className='util'>
-					<li>
-						<Link to='/etc'>
-							<FontAwesomeIcon icon={faHeart} />
-						</Link>
-					</li>
-					<li>
-						<Link to='/etc2'>
-							<FontAwesomeIcon icon={faUser} />
-						</Link>
-					</li>
-					<li>
-						<Link to='/etc3'>
-							<FontAwesomeIcon icon={faBagShopping} />
-						</Link>
-					</li>
-				</ul>
-
-				<NavLink to='#' className='btn_mo'>
-					<FontAwesomeIcon icon={faBars} />
-				</NavLink>
-			</div>
-		</header>
+					<NavLink to='#' className='btn_mo'>
+						<FontAwesomeIcon icon={faBars} onClick={() => toggleMenu.current.toggle()} />
+					</NavLink>
+				</div>
+			</header>
+			<Menu ref={toggleMenu} />
+		</>
 	);
 }
 
---
+++ src/components/common/Menu.js
@@ -0,0 +1,63 @@
+import { forwardRef, useImperativeHandle, useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Menu = forwardRef((props, ref) => {
+	const [Open, setOpen] = useState(false);
+	const active = { color: 'aqua' };
+
+	//윈도우 리사이즈 이벤트
+	useEffect(() => {
+		window.addEventListener('resize', () => {
+			if (window.innerWidth >= 1000) setOpen(false);
+		});
+	}, []);
+
+	useImperativeHandle(ref, () => {
+		return { toggle: () => setOpen(!Open) };
+	});
+
+	return (
+		<>
+			{Open && (
+				<nav id='mobilePanel' onClick={() => setOpen(false)}>
+					<h1>LOGO</h1>
+
+					<ul id='gnb'>
+						<li>
+							<NavLink to='/department' activeStyle={active}>
+								Department
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube' activeStyle={active}>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeStyle={active}>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/member' activeStyle={active}>
+								Member
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/contact' activeStyle={active}>
+								Contact
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeStyle={active}>
+								Community
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+			)}
+		</>
+	);
+});
+
+export default Menu;
