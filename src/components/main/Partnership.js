0bb2a59f8713fffbaed8cb4842ee5b6982279b33
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-22 22:50:15 +0900

    Partnership flickr 미리보기 이미지 출력

--- src/components/main/Partnership.js
+++ src/components/main/Partnership.js
@@ -1,34 +1,35 @@
+import { memo } from 'react';
+import { useSelector } from 'react-redux';
+
 function Partnership() {
+	const { flickr } = useSelector((store) => store.flickrReducer);
+
 	return (
 		<>
 			<section id='partnership' className='my_scroll'>
 				<div className='inner'>
 					<h1>Partnership</h1>
 					<div className='prs_wrap'>
-						<div className='item'>
-							<div className='name'>
-								<p>Lorem ipsum</p>
-								<div className='cross'>
-									<span className='bar row'></span>
-									<span className='bar col'></span>
+						{flickr.map((pic, idx) => {
+							if (idx >= 2) return null;
+							return (
+								<div className='item' key={pic.id}>
+									<div className='name'>
+										<p>{pic.title}</p>
+										<div className='cross'>
+											<span className='bar row'></span>
+											<span className='bar col'></span>
+										</div>
+									</div>
+									<article className='img_area'>
+										<img
+											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+											alt={pic.title}
+										/>
+									</article>
 								</div>
-							</div>
-							<article className='img_area'>
-								<img src={`${process.env.PUBLIC_URL}/img/main/prs1.jpg`} alt={'partnership'} />
-							</article>
-						</div>
-						<div className='item'>
-							<div className='name'>
-								<p>Lorem ipsum</p>
-								<div className='cross'>
-									<span className='bar row'></span>
-									<span className='bar col'></span>
-								</div>
-							</div>
-							<article className='img_area'>
-								<img src={`${process.env.PUBLIC_URL}/img/main/prs2.jpg`} alt={'partnership'} />
-							</article>
-						</div>
+							);
+						})}
 					</div>
 				</div>
 			</section>
@@ -36,4 +37,4 @@ function Partnership() {
 	);
 }
 
-export default Partnership;
+export default memo(Partnership);
