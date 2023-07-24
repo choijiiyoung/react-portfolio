743530d26faeed22f7ee359464087d2b23291bef
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-24 20:13:02 +0900

    메인 flickr 데이터 미리 보기 출력

--- src/components/main/Partnership.js
+++ src/components/main/Partnership.js
@@ -1,5 +1,40 @@
+import { memo } from 'react';
+import { useSelector } from 'react-redux';
+
 function Partnership() {
-	return <div>Partnership</div>;
+	const Flickr = useSelector((store) => store.flickr.data);
+	console.log(Flickr, 'Flickr');
+	return (
+		<>
+			<section id='partnership' className='my_scroll'>
+				<div className='inner'>
+					<h1>Partnership</h1>
+					<div className='prs_wrap'>
+						{Flickr.map((pic, idx) => {
+							if (idx >= 2) return null;
+							return (
+								<div className='item' key={pic.id}>
+									<div className='name'>
+										<p>{pic.title}</p>
+										<div className='cross'>
+											<span className='bar row'></span>
+											<span className='bar col'></span>
+										</div>
+									</div>
+									<article className='img_area'>
+										<img
+											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+											alt={pic.title}
+										/>
+									</article>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
 
-export default Partnership;
+export default memo(Partnership);
