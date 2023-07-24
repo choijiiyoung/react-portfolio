082dc6641f5a6f145aa904bfb98f3ced2590285a
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-24 18:30:38 +0900

    메인 유튜브 데이터 미리 보기 출력

--- src/components/main/Imprv.js
+++ src/components/main/Imprv.js
@@ -1,7 +1,70 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Imprv() {
-	return <div>Imprv</div>;
+	const youtube = useSelector((store) => store.youtube.data);
+	const [Index, setIndex] = useState(0);
+
+	return (
+		<section id='imprv' className='my_scroll'>
+			<div className='inner'>
+				<div className='content'>
+					<div className='title_area'>
+						<h1>Always Improving</h1>
+					</div>
+					<div className='txt_area'>
+						<p>
+							<b>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate magnam deleniti quas harum alias,
+								corporis ipsum
+							</b>
+						</p>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore explicabo sequi tenetur debitis esse
+							blanditiis accusantium quibusdam. Ducimus, necessitatibus aspernatur.
+						</p>
+					</div>
+				</div>
+				<div className='acco_wrap'>
+					<ul className='list'>
+						{youtube.map((vid, idx) => {
+							if (idx >= 3) return null;
+							return (
+								<li onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''} key={idx}>
+									<Link to='#'>
+										<span className='num'>0{idx + 1}</span>
+										<p>{vid.snippet.title}</p>
+										<div className='cross'>
+											<span className='bar row'></span>
+											<span className='bar col'></span>
+										</div>
+									</Link>
+									<div className='box'>
+										<article className='img_area'>
+											<img src={vid.snippet.thumbnails.medium.url} alt={vid.snippet.title} />
+										</article>
+										<div className='txt'>
+											<p>
+												{vid.snippet.description.length > 300
+													? vid.snippet.description.substr(0, 300) + '...'
+													: vid.snippet.description}
+											</p>
+										</div>
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<div className='btn_wrap'>
+					<Link to='#' className='btn'>
+						Explore our history
+					</Link>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default memo(Imprv);
