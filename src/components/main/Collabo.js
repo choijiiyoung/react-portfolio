0b9985685b2ff9214df07beaeb8a5198549748a9
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-24 19:23:55 +0900

    collabo 영역 scheduls 데이터 미리 보기 출력

--- src/components/main/Collabo.js
+++ src/components/main/Collabo.js
@@ -1,7 +1,47 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Collabo() {
-	return <div>Collabo</div>;
+	const Members = useSelector((store) => store.department.data);
+	const [Index, setIndex] = useState(0);
+	return (
+		<>
+			<section id='collabo' className='my_scroll'>
+				<div className='inner'>
+					<h1>Collaborations</h1>
+					<div className='collabo_wrap'>
+						<ul className='collabo_list'>
+							{Members.map((member, idx) => {
+								return (
+									<li key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
+										<Link to='#'>
+											<span className='name'>{member.name}</span>
+										</Link>
+										<div className='box'>
+											<div className='txt_area'>
+												<span className='name'>{member.name}</span>
+												<div className='cross'>
+													<span className='bar row'></span>
+													<span className='bar col'></span>
+												</div>
+											</div>
+
+											<article className='img_wrap'>
+												<div className='img_area'>
+													<img src={`${process.env.PUBLIC_URL}/img/department/${member.pic}`} alt={'member'} />
+												</div>
+											</article>
+										</div>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default memo(Collabo);
