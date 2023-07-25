04e3647bfe4284963696a0b91d971c9009f27036
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-25 21:08:37 +0900

    메모리 누수 오류 해결

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,26 +1,29 @@
 import { useRef, useEffect, useState } from 'react';
 import Anime from '../../asset/anime';
+import { useCallback } from 'react';
 
 function Btns() {
 	const btnRef = useRef(null);
 	const pos = useRef([]);
 	const [Num, setNum] = useState(0);
+	const [Mounted, setMounted] = useState(true);
 
-	const getPos = () => {
+	const getPos = useCallback(() => {
 		pos.current = [];
-		const secs = btnRef.current.parentElement.querySelectorAll('.my_scroll');
-		for (const sec of secs) pos.current.push(sec.offsetTop);
+		const secs = btnRef.current?.parentElement.querySelectorAll('.my_scroll');
+		if (!secs) return;
 
-		setNum(pos.current.length);
-	};
+		for (const sec of secs) pos.current?.push(sec.offsetTop);
+		Mounted && setNum(pos.current?.length);
+	}, [Mounted]);
 
 	const activation = () => {
 		const base = -window.innerHeight / 2;
 		const scroll = window.scrollY;
-		const btns = btnRef.current.children;
-		const boxs = btnRef.current.parentElement.querySelectorAll('.my_scroll');
+		const btns = btnRef.current?.children;
+		const boxs = btnRef.current?.parentElement.querySelectorAll('.my_scroll');
 
-		pos.current.forEach((pos, idx) => {
+		pos.current?.forEach((pos, idx) => {
 			if (scroll >= pos + base) {
 				for (const btn of btns) btn.classList.remove('on');
 				btns[idx].classList.add('on');
@@ -29,29 +32,33 @@ function Btns() {
 		});
 	};
 
-	const visualEvt = () => {
-		const visual = btnRef.current.parentElement.querySelector('#visual');
-		visual.classList.add('active');
-	};
+	const visualEvt = useCallback(() => {
+		const visual = btnRef.current?.parentElement.querySelector('#visual');
+		if (Mounted) {
+			visual?.classList.add('active');
+		}
+	}, [Mounted]);
 
 	useEffect(() => {
+		setTimeout(() => {
+			visualEvt();
+		}, 500);
+
 		setTimeout(() => {
 			getPos();
 		}, 1000);
 
-		visualEvt();
 		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', activation);
-		window.addEventListener('load', () => {
-			setTimeout(visualEvt, 500);
-		});
 
 		return () => {
+			setMounted(false);
+
 			window.removeEventListener('resize', getPos);
 			window.removeEventListener('scroll', activation);
 			window.removeEventListener('load', visualEvt);
 		};
-	}, []);
+	}, [getPos, visualEvt]);
 
 	return (
 		<ul className='btn_navi' ref={btnRef}>
