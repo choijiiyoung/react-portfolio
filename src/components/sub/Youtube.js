e719de2fd1bd3c8e2b8ef7006b22202719aa3675
choijiiyoung <blueberryxchoco@gmail.com>
2023-08-08 12:44:27 +0900

    새로고침시 slide안에 생기는 null값 안나오도록 소스 수정

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -41,8 +41,9 @@ function Youtube() {
 	};
 
 	useEffect(() => {
-		frame.current.append(frame.current.firstElementChild);
-		frame.current.append(frame.current.firstElementChild);
+		const slideState = frame.current.firstElementChild;
+		slideState && frame.current.append(frame.current.firstElementChild);
+		slideState && frame.current.append(frame.current.firstElementChild);
 	}, []);
 
 	return (
