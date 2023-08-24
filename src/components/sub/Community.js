e467f7f71110c90950e4f002deb0147fe05ce393
choijiiyoung <blueberryxchoco@gmail.com>
2023-08-24 18:29:07 +0900

    주석 추가

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,6 +2,8 @@ import Layout from '../common/Layout';
 import { useRef, useState, useEffect } from 'react';
 
 function Community() {
+	//로컬저장소의 데이터를 반환하는 함수
+	//저장소에 값이 있으면 해당 값을 다시 JSON형태로 변경해서 반환
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
 		return JSON.parse(data);
@@ -12,7 +14,7 @@ function Community() {
 	const editInput = useRef(null);
 	const editTextarea = useRef(null);
 	const [Posts, setPosts] = useState(getLocalData());
-	const [Allowed, setAllowed] = useState(true);
+	const [Allowed, setAllowed] = useState(true); //복수개 글 수정 진입불가
 
 	//리셋
 	const resetForm = () => {
@@ -20,23 +22,29 @@ function Community() {
 		textarea.current.value = '';
 	};
 
+	//Create - 데이터 저장 (개시글 저장)
 	const createPost = () => {
 		if (!input.current.value.trim() || !textarea.current.value.trim()) {
 			resetForm();
 			return alert('제목과 본문을 모두 입력하세요.');
 		}
 
+		//Read - 데이터 호출 (게시글 보기)
 		setPosts([{ title: input.current.value, content: textarea.current.value }, ...Posts]);
 		resetForm();
 	};
 
+	//Delete - 데이터 삭제 (게시글 삭제)
 	const deletePost = (delIndex) => {
 		if (!window.confirm('해당 게시글을 삭제하겠습니까?')) return;
 		setPosts(Posts.filter((_, idx) => idx !== delIndex));
 	};
 
+	//Update(Edit) - 데이터 수정 (게시글 수정)
 	const enableUpdate = (editIndex) => {
+		//수정모드 진입함수 호출시 Allowed가 true일때에만 로직이 실행되도록 처리
 		if (!Allowed) return;
+		//일단 로직이 실행되면 Allowed값을 false로 바꿔서 이후부터는 다시 수정모드로 진입되는 것을 방지
 		setAllowed(false);
 
 		setPosts(
@@ -54,9 +62,11 @@ function Community() {
 				return post;
 			})
 		);
+		//글 수정 취소버튼을 눌러서 disableUpdate함수가 호출이 되어야지만 Allowed값을 다시 true로 바꿔서 글 수정 가능하게 처리
 		setAllowed(true);
 	};
 
+	//Update - 데이터 수정 (게시글 수정)
 	const updatePost = (editIndex) => {
 		if (!editInput.current.value.trim() || !editTextarea.current.value.trim()) {
 			return alert('수정할 제목과 본문을 모두 입력하세요.');
@@ -76,6 +86,7 @@ function Community() {
 	};
 
 	useEffect(() => {
+		//Posts State값이 변경될때마다 해당 데이터를 문자화해서 localStorage에 저장
 		localStorage.setItem('post', JSON.stringify(Posts));
 	}, [Posts]);
 
