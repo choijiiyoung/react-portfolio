2b395e7185a7c6f2baf739a753a84b231408cd7c
choijiiyoung <blueberryxchoco@gmail.com>
2023-09-04 21:51:55 +0900

    주석 추가

--- src/hooks/useDepartmentQuery.js
+++ src/hooks/useDepartmentQuery.js
@@ -8,8 +8,8 @@ const fetchDepartment = async () => {
 
 export const useDepartmentQuery = () => {
 	return useQuery(['departmentData'], fetchDepartment, {
-		refetchOnMount: false,
-		refetchOnWindowFocus: false,
+		refetchOnMount: false, //윈도우 포커스갈때마다 패칭되는거 막아줌
+		refetchOnWindowFocus: false, //불필요한 리패칭 방지
 		cacheTime: 1000 * 60 * 60 * 24,
 		staleTime: 1000 * 60 * 60 * 24,
 	});
